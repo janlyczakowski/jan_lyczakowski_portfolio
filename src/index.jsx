0e942d849f4ba101cd586ee8185de07d3c438972
lyczakowskijan <lyczakowski.jan@gmail.com>
2024-02-02 10:48:06 +0100

    initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -80,6 +80,6 @@ const router = createBrowserRouter([
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} basename="/jan_lyczakowski_portfolio" />
   </React.StrictMode>
 );
