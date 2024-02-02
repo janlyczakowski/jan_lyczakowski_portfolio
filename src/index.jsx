1fd609786845639c5e34b63a04fc21d9320e4067
lyczakowskijan <lyczakowski.jan@gmail.com>
2024-02-02 10:54:18 +0100

    initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutMe from './pages/AboutMe';
 import Contact from './pages/Contact';
@@ -19,7 +19,7 @@ import {
   PORTFOLIO_PAGE_1,
 } from './utils/constants';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <Home />,
